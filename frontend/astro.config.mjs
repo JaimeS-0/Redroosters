69ec2166d9150332b01a3b906c5397eab213fe90
Jaime <jaimecontactdev@gmail.com>
2026-08-04 20:39:51 +0200

    update

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,4 +1,4 @@
-/*
+
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
@@ -21,33 +21,6 @@ export default defineConfig({
     },
   },
 });
-*/
 
-////////////////////////
-
-
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
-
-export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
-  integrations: [
-    tailwind(),
-  ],
-  vite: {
-    server: {
-      proxy: {
-        '/api/': {
-          target: 'http://backend:9000',
-          changeOrigin: true,
-        },
-      },
-    },
-  },
-});
 
 
