6805cc60bc5026aa771a09aeb09a27ba54c32c08
Jaime <jaimecontactdev@gmail.com>
2026-08-07 15:07:58 +0200

    Error no cargar BBDD

--- frontend/src/config/backend.js
+++ frontend/src/config/backend.js
@@ -1,2 +1,15 @@
-export const PUBLIC_BACKEND_URL = import.meta.env.PUBLIC_BACKEND_URL;
-export const INTERNAL_BACKEND_URL = import.meta.env.BACKEND_INTERNAL_URL;
+//export const PUBLIC_BACKEND_URL = import.meta.env.PUBLIC_BACKEND_URL;
+//export const INTERNAL_BACKEND_URL = import.meta.env.BACKEND_INTERNAL_URL;
+
+// Para peticiones desde el navegador (cliente)
+export const PUBLIC_BACKEND_URL = 
+  import.meta.env.PUBLIC_BACKEND_URL || 
+  process.env.PUBLIC_BACKEND_URL || 
+  "";
+
+// Para peticiones desde el servidor (SSR Node en Docker)
+export const INTERNAL_BACKEND_URL = 
+  import.meta.env.BACKEND_INTERNAL_URL || 
+  process.env.BACKEND_INTERNAL_URL || 
+  process.env.INTERNAL_BACKEND_URL || 
+  "http://backend:9000";
